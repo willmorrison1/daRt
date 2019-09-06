994ed91c82dc4a7175744342e2bb42f2753cc439
kitbenjamin <kitbenjamin@googlemail.com>
2019-09-06 10:22:34 +0100

    Time presented in seconds rather than datetime object as this has potential for errors e.g. if time taken > 24 hours

--- R/resourceUse.R
+++ R/resourceUse.R
@@ -20,6 +20,10 @@ setMethod("resourceUse", "SimulationFiles",
     for (i in 1:length(dartTxtFiles)) {
         rawFileDATA <- readLines(dartTxtFiles[i], warn = FALSE)
         timeTaken <- searchDartTxtVal(rawFileDATA, searchQuote = "Processing time")
+        timeTakenSplit <- lapply(timeTaken, function(x){
+            as.numeric(strsplit(x, split = ' ')[[1]][-1])})
+        timeTakenSecs <- unlist(lapply(timeTakenSplit, function(x){
+            sum(x[1]*60*60, x[2]*60, x[3])}))
 
         #creates a time class
         #timeTaken <- unlist(lapply(timeTaken, function(x){paste0(strsplit(x, split = ' ')[[1]], collapse = ':')}))
@@ -31,7 +35,7 @@ setMethod("resourceUse", "SimulationFiles",
         memUsed <- searchDartTxtVal(rawFileDATA, searchQuote = "Memory usage")
         memUsed <- as.numeric(memUsed)
         outList[[i]] <- data.frame("simName" = simNames[i],
-                                   "timeTaken" = timeTaken,
+                                   "timeTaken" = timeTakenSecs,
                                    "memUsage" = memUsed,
                                    stringsAsFactors = FALSE)
     }
