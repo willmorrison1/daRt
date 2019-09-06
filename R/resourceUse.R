9d78dd5229d7d9355737060c6722a5c6618b2d08
kitbenjamin <kitbenjamin@googlemail.com>
2019-09-06 10:22:34 +0100

    Time presented in seconds rather than datetime object as this has potential for errors e.g. if time taken > 24 hours

--- R/resourceUse.R
+++ R/resourceUse.R
@@ -1,14 +1,3 @@
-# how to get x - for below
-# library(daRt)
-# setwd("V:/Tier_processing/hv867657/DART_5-7-5_1126/user_data/simulations/SPARTACUS_SW/daRtinput/2019241_233930/")
-# simDir <- c("sequence1o_0","sequence1o_1")
-# sF <- simulationFilter(product = "directions", iters = "ITERX")
-# simFiles <- daRt::getFiles(x = simDir, sF = sF)
-# x <- daRt::getData(simFiles)
-# resourceUse(x)
-#remove these comments when you have finished debugging
-#
-
 #' @export
 setMethod("resourceUse", "SimulationFiles",
           function(x){
@@ -20,6 +9,10 @@ setMethod("resourceUse", "SimulationFiles",
     for (i in 1:length(dartTxtFiles)) {
         rawFileDATA <- readLines(dartTxtFiles[i], warn = FALSE)
         timeTaken <- searchDartTxtVal(rawFileDATA, searchQuote = "Processing time")
+        timeTakenSplit <- lapply(timeTaken, function(x){
+            as.numeric(strsplit(x, split = ' ')[[1]][-1])})
+        timeTakenSecs <- unlist(lapply(timeTakenSplit, function(x){
+            sum(x[1]*60*60, x[2]*60, x[3])}))
 
         #creates a time class
         #timeTaken <- unlist(lapply(timeTaken, function(x){paste0(strsplit(x, split = ' ')[[1]], collapse = ':')}))
@@ -31,7 +24,7 @@ setMethod("resourceUse", "SimulationFiles",
         memUsed <- searchDartTxtVal(rawFileDATA, searchQuote = "Memory usage")
         memUsed <- as.numeric(memUsed)
         outList[[i]] <- data.frame("simName" = simNames[i],
-                                   "timeTaken" = timeTaken,
+                                   "timeTaken" = timeTakenSecs,
                                    "memUsage" = memUsed,
                                    stringsAsFactors = FALSE)
     }
