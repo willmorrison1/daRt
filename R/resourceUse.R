74e8bb89a5cc8809830b4bc1852f93698d3e7372
willmorrison1 <willmorrison661@gmail.com>
2019-10-04 21:59:05 +0100

    man

--- R/resourceUse.R
+++ R/resourceUse.R
@@ -1,36 +1,40 @@
+#' Get compute resource usage
+#' @title resourceUse
+#' @description Return data frame with simulation name, memory usage and time taken
+#' @param SimulationFiles \link{SimulationFiles-class} type object
+#'
+#' @return
 #' @export
+#'
+#' @examples
 setMethod("resourceUse", "SimulationFiles",
           function(x){
-    require(dplyr)
-    require(chron)
-    dartTxtFiles <- getDartTxtFileName(x)
-    outList <- vector("list", length = length(dartTxtFiles))
-    simNames <- simname(x)
-    for (i in 1:length(dartTxtFiles)) {
-        rawFileDATA <- readLines(dartTxtFiles[i], warn = FALSE)
-        timeTaken <- searchDartTxtVal(rawFileDATA, searchQuote = "Processing time")
-        timeTakenSplit <- lapply(timeTaken, function(x){
-            as.numeric(strsplit(x, split = ' ')[[1]][-1])})
-        timeTakenSecs <- unlist(lapply(timeTakenSplit, function(x){
-            sum(x[1]*60*60, x[2]*60, x[3])}))
+              require(dplyr)
+              require(chron)
+              dartTxtFiles <- getDartTxtFileName(x)
+              outList <- vector("list", length = length(dartTxtFiles))
+              simNames <- simname(x)
 
-        #creates a time class
-        #timeTaken <- unlist(lapply(timeTaken, function(x){paste0(strsplit(x, split = ' ')[[1]], collapse = ':')}))
-        #timeTaken <- chron(times = timeTaken, format = 'h:m:s')
+              for (i in 1:length(dartTxtFiles)) {
+                  rawFileDATA <- readLines(dartTxtFiles[i], warn = FALSE)
+                  timeTaken <- searchDartTxtVal(rawFileDATA, searchQuote = "Processing time")
+                  timeTakenSplit <- lapply(timeTaken, function(x){
+                      as.numeric(strsplit(x, split = ' ')[[1]][-1])})
+                  timeTakenSecs <- unlist(lapply(timeTakenSplit, function(x)
+                      sum(x[1] * 60*60, x[2] * 60, x[3])))
 
-        #if posixct preferred but time is preeded by date
-        #timeTaken <- as.POSIXct(timeTaken, format = '%H %M %S')
+                  memUsed <- searchDartTxtVal(rawFileDATA, searchQuote = "Memory usage")
+                  memUsed <- as.numeric(memUsed)
+                  outList[[i]] <- data.frame("simName" = simNames[i],
+                                             "timeTaken" = timeTakenSecs,
+                                             "memUsage" = memUsed,
+                                             stringsAsFactors = FALSE)
+              }
 
-        memUsed <- searchDartTxtVal(rawFileDATA, searchQuote = "Memory usage")
-        memUsed <- as.numeric(memUsed)
-        outList[[i]] <- data.frame("simName" = simNames[i],
-                                   "timeTaken" = timeTakenSecs,
-                                   "memUsage" = memUsed,
-                                   stringsAsFactors = FALSE)
-    }
-    outDF <- dplyr::bind_rows(outList)
-    return(outDF)
-})
+              outDF <- dplyr::bind_rows(outList)
+              return(outDF)
+          })
+
 
 searchDartTxtVal <- function(rawFileData, searchQuote) {
     rawVal <- strsplit(rawFileData[grepl(searchQuote, rawFileData)], "[*]")
